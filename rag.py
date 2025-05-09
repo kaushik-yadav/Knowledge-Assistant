e463d5dab94a549bb28f1ed74400d6166b94d191
kaushik-yadav <kaushiky005@gmail.com>
2025-05-09 19:10:04 +0530

    feat: Integrate RAG chain with prompt and LLM execution

--- rag.py
+++ rag.py
@@ -1,10 +1,18 @@
 import os
 import re
 
+from dotenv import load_dotenv
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.schema import Document
 from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable
 from langchain_core.vectorstores import VectorStoreRetriever
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
 
 docs = {}
 # taking all the text files present in docs
@@ -54,7 +62,7 @@ embedding_model = HuggingFaceEmbeddings(
 # this embeds the docs and builds a FAISS index
 vector_store = FAISS.from_documents(documents, embedding_model)
 
-# save the index in docs/
+# save the index in data/
 vector_store.save_local("data/documents_index")
 
 # loading the model
@@ -73,3 +81,35 @@ retriever = VectorStoreRetriever(
 top_k_queries = retriever.get_relevant_documents(
     "How does the policy of returning works in this company?"
 )
+
+# initiating the LLM with model name and API credentials using an OPENAI wrapper
+llm = ChatOpenAI(
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
+    openai_api_base=os.getenv("OPENAI_API_BASE"),
+    model_name="mistralai/Mistral-7B-Instruct-v0.1",
+)
+# creating a system prompt for the agent
+system_prompt = (
+    "Use the given context to answer the question. "
+    "If you don't know the answer, say 'I don't know'. "
+    "Use three sentences maximum. Be concise.\n\n"
+    "Context:\n{context}"
+)
+# defining the prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [("system", system_prompt), ("human", "{input}")]
+)
+
+# creating chain of actions for documents
+# here we setup a question answer chain
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+
+# combining the document retriever with the question answer chain that we created above
+# basically creating a RAG chain by combining the retriever and our question answer chain
+rag_chain: Runnable = create_retrieval_chain(retriever, question_answer_chain)
+
+query = "which products does the company provide?"
+# executing the RAG chain
+response = rag_chain.invoke({"input": query})
+
+print(response["answer"])
