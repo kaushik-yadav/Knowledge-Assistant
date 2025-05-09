7cf9d9a50468ab4de69436a2aa3ede6e7efe72ee
kaushik-yadav <kaushiky005@gmail.com>
2025-05-09 20:13:46 +0530

    feat: Add RAG pipeline with document loading, chunking, and FAISS retriever setup

---
+++ src/rag.py
@@ -0,0 +1,34 @@
+from langchain_community.vectorstores import FAISS
+from langchain_core.vectorstores import VectorStoreRetriever
+from langchain_huggingface import HuggingFaceEmbeddings
+
+from config import EMBED_MODEL_NAME, INDEX_PATH
+from utils import get_documents
+
+
+def start_rag():
+
+    documents = get_documents()
+
+    # loading the embedding model
+    embedding_model = HuggingFaceEmbeddings(model_name=f"{EMBED_MODEL_NAME}")
+
+    # this embeds the docs and builds a FAISS index
+    vector_store = FAISS.from_documents(documents, embedding_model)
+
+    # save the index in data/
+    vector_store.save_local(INDEX_PATH)
+
+    # loading the FAISS index
+    vector_store = FAISS.load_local(
+        INDEX_PATH,
+        embeddings=embedding_model,
+        allow_dangerous_deserialization=True,
+    )
+
+    # initializing the retriever to retrieve the top k relevant documents with a nearest neighbors search (fetching up to 50 candidates)
+    retriever = VectorStoreRetriever(
+        vectorstore=vector_store, search_kwargs={"k": 6, "fetch_k": 50}
+    )
+
+    return retriever
